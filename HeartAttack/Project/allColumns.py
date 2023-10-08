1c90432ea83c926275b8529145bfe5ff11365c81
Md Bikasuzzaman <66817101+Bikas0@users.noreply.github.com>
2023-10-08 13:12:46 +0600

    Add files via upload

---
+++ HeartAttack/Project/allColumns.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class Features(BaseModel):
+    age: int
+    sex: int
+    cp: int
+    trtbps: int
+    chol: int
+    fbs: int
+    restecg: int
+    thalachh: int
+    exng: int
+    oldpeak: float
+    slp: int
+    caa: int
+    thall: int
